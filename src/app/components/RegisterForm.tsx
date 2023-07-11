a997d392e997e4b08c92d62e5c1f4c078acdeee7
AmletixQ <nazarovamridin308@gmail.com>
2023-07-11 19:01:17 +0300

    Create register page and its functional with types

---
+++ src/app/components/RegisterForm.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { ChangeEvent, useState } from "react";
+import Button from "./UI/Button";
+import Input from "./UI/Input";
+import { ISignInData } from "../interfaces/interfaces";
+
+const RegisterForm = () => {
+  const [signInData, setSignInData] = useState<ISignInData>({} as ISignInData);
+
+  return (
+    <form>
+      <Input
+        placeholder="Enter your username"
+        type="text"
+        value={signInData.username}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setSignInData({ ...signInData, username: e.target.value })
+        }
+      />
+      <Input
+        placeholder="Enter your email"
+        type="email"
+        value={signInData.email}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setSignInData({ ...signInData, email: e.target.value })
+        }
+      />
+      <Input
+        placeholder="Enter your password"
+        type="text"
+        value={signInData.password}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setSignInData({ ...signInData, password: e.target.value })
+        }
+      />
+      <Button>Register</Button>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -2,3 +2,7 @@ export interface ILogInData {
   email: string;
   password: string;
 }
+
+export interface ISignInData extends ILogInData {
+  username: string;
+}

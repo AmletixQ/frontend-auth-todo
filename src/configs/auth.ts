d6f3ab73637cd11dd9f46c38a1f689ccd6ae47ee
AmletixQ <nazarovamridin308@gmail.com>
2023-07-13 18:43:22 +0300

    Create authentification with GitHub API

---
+++ src/configs/auth.ts
@@ -0,0 +1,12 @@
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authConfig: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  secret: process.env.SECRET,
+};

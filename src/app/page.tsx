2758f7f4f0922f76612f1d1031174e80fab0745b
AmletixQ <nazarovamridin308@gmail.com>
2023-07-13 18:49:22 +0300

    Update page files for modifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import Button from "@/components/UI/Button";
+import { signIn, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const RootPage = () => {
-  return <div>Hello!</div>;
+  const { data: session, status } = useSession();
+
+  if (status === "authenticated") {
+    redirect("/profile");
+  }
+
+  return (
+    <div>
+      <h1>Not in signed!</h1>
+      <Button onClick={() => signIn()}>Sign in</Button>
+    </div>
+  );
 };
 
 export default RootPage;

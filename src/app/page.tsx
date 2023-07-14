56c0bfee1654704293ec3d83eb6c7e9691ba55ad
AmletixQ <nazarovamridin308@gmail.com>
2023-07-14 17:35:52 +0300

    Rebuild main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+import Button from "@/components/UI/Button";
+import Span from "@/components/UI/Span";
+import { useRouter } from "next/navigation";
+import { styled } from "styled-components";
+
+const StyledBlock = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const RootPage = () => {
+  const router = useRouter();
   return (
-    <>
-      <h1>Root page</h1>
-    </>
+    <StyledBlock>
+      <Button onClick={() => router.push("/register")}>Register</Button>
+      <Span> or </Span>
+      <Button onClick={() => router.push("/login")}>Login</Button>
+    </StyledBlock>
   );
 };
 

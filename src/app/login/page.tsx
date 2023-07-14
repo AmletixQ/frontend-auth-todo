4562f1ce725c306a90f966c404e25ed29dba9e35
AmletixQ <nazarovamridin308@gmail.com>
2023-07-14 12:59:40 +0300

    Some fixes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,6 @@
 import LoginForm from "@/components/LoginForm";
-import { authConfig } from "@/configs/auth";
 
 const LoginPage = () => {
-  console.log(authConfig);
   return (
     <div>
       <h1>Login page</h1>

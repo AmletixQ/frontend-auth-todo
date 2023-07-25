dd690e0aa1e24e5ba59ce81d081f9525cee55ba7
AmletixQ <nazarovamridin308@gmail.com>
2023-07-25 21:11:56 +0300

    Add style

--- src/components/UI/StyledHeader.tsx
+++ src/components/UI/StyledHeader.tsx
@@ -4,6 +4,7 @@ import { styled } from "styled-components";
 const StyledHeader = styled.header`
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 20px;
   padding: 5px 20px;
   margin-bottom: 20px;

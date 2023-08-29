d370b96ecb5f88c7ec3533e6752ba0d09f2f1836
AmletixQ <nazarovamridin308@gmail.com>
2023-08-29 15:43:26 +0300

    Rechange environment data

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const db = new Pool({
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT!,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.POSTGRES_HOST,
+  port: +process.env.POSTGRES_PORT!,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_USER,
 });
 
 export default db;

99bb977c3152194f0d83ffc92f0498a05ab93e8c
AmletixQ <nazarovamridin308@gmail.com>
2023-10-29 21:30:01 +0300

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface ILink {
+  href: string;
+  name: string;
+}
+
+const links: ILink[] = [
+  { href: "/dashboard", name: "Home" },
+  { href: "/dashboard/settings", name: "Settings" },
+  { href: "/dashboard/todos", name: "Todos" },
+];
+
+const Header = () => {
+  const path = usePathname();
+
+  return (
+    <div className="flex justify-evenly py-4 border-b border-slate-700">
+      {links.map(({ href, name }) => (
+        <Link
+          className={
+            "border border-slate-500 px-2 py-1 rounded-md" +
+            (path === href ? " border-yellow-500" : "")
+          }
+          href={href}
+          key={href + name}
+        >
+          {name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Header;

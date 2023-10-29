6d047c5ea3a46cc5a0423249f79f8d9dac2c9d29
AmletixQ <nazarovamridin308@gmail.com>
2023-10-29 21:45:20 +0300

    Create new endpoints

---
+++ src/app/(profile)/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProfileLoader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-2xl font-bold text-yellow-500">
+        Profile loading ...
+      </h1>
+    </div>
+  );
+};
+
+export default ProfileLoader;

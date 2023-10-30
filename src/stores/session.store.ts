4987c0c87c224b380588a1b8fca4738f8762351a
AmletixQ <nazarovamridin308@gmail.com>
2023-10-30 13:24:03 +0300

    Add persist wrapper

--- src/stores/session.store.ts
+++ src/stores/session.store.ts
@@ -2,23 +2,29 @@ import { http } from "@/lib/http";
 import ISession from "@/types/session/session.interface";
 import ISessionStore from "@/types/session/sessionStore.interface";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useSession = create<ISessionStore>((set) => ({
-  session: {
-    id: undefined,
-    username: undefined,
-    password: undefined,
-  },
-  entrance: async (_data, url) => {
-    const {
-      data: { user },
-    } = await http.post<{ user: ISession }>(url, {
-      user: _data,
-    });
-    set(() => ({ session: user }));
-  },
-  logout: async () => {
-    await http.post("/users/logout");
-  },
-  update: () => {},
-}));
+export const useSession = create<ISessionStore>()(
+  persist(
+    (set) => ({
+      session: {
+        id: undefined,
+        username: undefined,
+        password: undefined,
+      },
+      entrance: async (_data, url) => {
+        const {
+          data: { user },
+        } = await http.post<{ user: ISession }>(url, {
+          user: _data,
+        });
+        set((state) => ({ session: user }));
+      },
+      logout: async () => {
+        await http.post("/users/logout");
+      },
+      update: () => {},
+    }),
+    { name: "session", skipHydration: true },
+  ),
+);

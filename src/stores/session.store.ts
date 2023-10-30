2598cf6efb960e3d4c747a2cc0927baa284a1e8e
AmletixQ <nazarovamridin308@gmail.com>
2023-10-30 12:59:02 +0300

    Add Zustand store for session

---
+++ src/stores/session.store.ts
@@ -0,0 +1,23 @@
+import { http } from "@/lib/http";
+import ISession from "@/types/session/session.interface";
+import ISessionStore from "@/types/session/sessionStore.interface";
+import { create } from "zustand";
+
+export const useSession = create<ISessionStore>((set) => ({
+  session: {
+    id: undefined,
+    username: undefined,
+    password: undefined,
+  },
+  save: (data) => set(() => ({ session: data })),
+  entrance: async (_data, url) => {
+    const {
+      data: { user },
+    } = await http.post<{ user: ISession }>(url, {
+      user: _data,
+    });
+    set(() => ({ session: user }));
+  },
+  logout: async () => {},
+  update: () => {},
+}));

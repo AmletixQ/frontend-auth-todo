17a7de2c10a7891fbf3e94caf2263dbe0ff87f25
AmletixQ <nazarovamridin308@gmail.com>
2023-11-02 12:38:20 +0300

    Reconfig on server logic

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,6 +1,15 @@
 import axios from "axios";
+import { getCookies } from "./getCookies";
 
 export const http = axios.create({
   baseURL: "http://localhost:3001",
   withCredentials: true,
+});
+
+http.interceptors.request.use((config) => {
+  if (!config.headers.get("cookie") && typeof window === "undefined") {
+    config.headers.set("cookie", getCookies().toString());
+  }
+
+  return config;
 });
